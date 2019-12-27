07d0e12eca0ec07e58eab4374c98214a062c73dc
Meredith Bashert <merbashert@gmail.com>
2019-12-27 17:31:51 -0500

    Added edit modals to fabrics and needles

--- crafty_app/src/components/Fabrics.js
+++ crafty_app/src/components/Fabrics.js
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Modal from 'react-bootstrap/Modal';
+import FabricEdit from './FabricEdit'
 
 const Fabrics = props => {
+
+    const [show, setShow] = useState(false);
+
+        const handleClose = (e) => {
+            setShow(false)
+        }
+
+        const handleShow = (e) => {
+            setShow(true)
+        }
+
     return (
       <React.Fragment>
         <div className="fabric-box">
         <img src={props.fabricData.picture} className="fabric-pic" alt="fabric"/>
         <h5>Length:{props.fabricData.length}</h5>
-        <h5>Main Color: {props.fabricData.main_color}</h5>
         <h5>Tags: {props.fabricData.tags}</h5>
 
+        <p className="btn btn-sm btn-primary" onClick={handleShow}>
+          Edit
+        </p>
+        <>
+
+
+  <Modal show={show} onHide={handleClose}>
+    <Modal.Header closeButton>
+      <Modal.Title>Edit</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+    <FabricEdit fabricData={props.fabricData} handleUpdateFabric={props.handleUpdateFabric} handleClose={handleClose}/>
+     </Modal.Body>
+  </Modal>
+</>
 
         <p onClick={() => {
             props.handleDeleteFabric(props.fabricData.id)
-        }}>delete post</p>
+        }}>Delete</p>
         </div>
       </React.Fragment>
     )
--- crafty_app/src/components/Needles.js
+++ crafty_app/src/components/Needles.js
@@ -1,39 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from 'react-bootstrap/Modal';
+import NeedleEdit from './NeedleEdit'
+import yarnball from '../assets/favicon-32x32.png'
+
 
 const Needles = props => {
-        return (
-            <React.Fragment>
-            <div className = "needle-box">
-            {props.needleData.size}
-            <table>
-            <thead></thead>
-            <tbody>
-            <tr>
-            <td>Straight</td>
-            <td>{(props.needleData.straight==='1')? 'X' : null}</td>
-            </tr>
-            <tr>
-            <td>Circular</td>
-            <td>{(props.needleData.circular==='1')? 'X' : null}</td>
-            </tr>
-            <tr>
-            <td>Double-point</td>
-            <td>{(props.needleData.doublepoint==='1')? 'X' : null}</td>
-            </tr>
-            </tbody>
-            </table>
-            <p onClick={() => {
-                props.handleUpdateNeedle(props.needleData)
-            }}>edit</p>
 
-            <p onClick={() => {
-                props.handleDeleteNeedle(props.needleData.id)
-            }}>delete</p>
+    const [show, setShow] = useState(false);
 
-            </div>
-            <br/>
-            </React.Fragment>
-        )
+    const handleClose = (e) => {
+        setShow(false)
+    }
+
+    const handleShow = (e) => {
+        setShow(true)
+    }
+
+    return (
+        <React.Fragment>
+        <div className = "needle-box">
+        {props.needleData.size}
+        <table>
+        <thead></thead>
+        <tbody>
+        <tr>
+        <td>Straight</td>
+        <td>{(props.needleData.straight==='1')? <img src={yarnball}/> : null}</td>
+        </tr>
+        <tr>
+        <td>Circular</td>
+        <td>{(props.needleData.circular==='1')? <img src={yarnball}/> : null}</td>
+        </tr>
+        <tr>
+        <td>Double-point</td>
+        <td>{(props.needleData.doublepoint==='1')? <img src={yarnball}/> : null}</td>
+        </tr>
+        </tbody>
+        </table>
+        <p className="btn btn-sm btn-primary" onClick={handleShow}>
+        Edit
+        </p>
+        <>
+
+
+        <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+        <Modal.Title>Edit</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <NeedleEdit needleData={props.needleData} handleUpdateNeedle={props.handleUpdateNeedle} handleClose={handleClose}/>
+        </Modal.Body>
+        </Modal>
+        </>
+        <p onClick={() => {
+            props.handleDeleteNeedle(props.needleData.id)
+        }}>delete</p>
+
+        </div>
+        <br/>
+        </React.Fragment>
+    )
 }
 
 export default Needles
