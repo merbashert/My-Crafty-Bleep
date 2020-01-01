138870ca2ee2590c2cc5a8ef53f8db70f3189963
Meredith Bashert <merbashert@gmail.com>
2020-01-01 10:11:14 -0500

    Unactive random boxes fade out, default box 1

--- crafty_app/src/components/RandomPage.js
+++ crafty_app/src/components/RandomPage.js
@@ -12,7 +12,7 @@ import box_picture3 from '../assets/box3.png'
 const RandomPage = props => {
     const[randoms, setRandoms] = useState([])
     const[randomFilter, setRandomFilter] = useState('')
-    const[boxNumberFilter, setBoxNumberFilter] = useState('')
+    const[boxNumberFilter, setBoxNumberFilter] = useState('1')
 
 
     const handleChange = (e) => {
@@ -78,8 +78,9 @@ const RandomPage = props => {
         fetchRandom();
     }, [])
 
+
     return (
-        <>
+        <div className='random-page'>
         <div className="random-add">
         <RandomForm
         handleCreateRandom={handleCreateRandom}
@@ -106,9 +107,12 @@ const RandomPage = props => {
         </div>
 
         <div className="random-box">
-        <img src={box_picture1} alt="box 1" onClick={() => setBox('1')} className='boxpicture' id='1'/>
-        <img src={box_picture2} alt="box 2" onClick={() => setBox('2')} className='boxpicture'id='2'/>
-        <img src={box_picture3} alt="box 3" onClick={() => setBox('3')} className='boxpicture'id='3'/>
+
+        <div className='boxes'>
+        <img src={box_picture1} alt="box 1" onClick={() => setBox('1')} className='boxpicture' id='1' style={{opacity: boxNumberFilter!='1'?'50%':'100%'}}/>
+        <img src={box_picture2} alt="box 2" onClick={() => setBox('2')} className='boxpicture'id='2' style={{opacity: boxNumberFilter!='2'?'50%':'100%'}}/>
+        <img src={box_picture3} alt="box 3" onClick={() => setBox('3')} className='boxpicture'id='3' style={{opacity: boxNumberFilter!='3'?'50%':'100%'}}/>
+        </div>
 
         {boxNumberFilter?<h1>Box {boxNumberFilter}</h1>:null}
 
@@ -128,7 +132,7 @@ const RandomPage = props => {
         </tbody>
         </Table>
         </div>
-        </>
+        </div>
 
 
 
