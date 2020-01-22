3f797b93330955d93a5fea615cdca66cd3fe20df
Meredith Bashert <merbashert@gmail.com>
2020-01-22 11:44:03 -0500

    Trying to fix banner

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import banner from '../assets/mycraftybleep.jpg'
 import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
 
 
 const Home = props => {
 
     return (
         <div className="sitename-container">
-         <Image src={banner} alt='banner'/>
-         </div>
+            
+                <Image src={banner} alt='banner'/>
+            
+        </div>
     )
-  }
+}
 export default Home
