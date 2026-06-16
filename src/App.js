20524601db4a367144288013eb86cd3a246ca66a
Meredith Bashert <54551786+merbashert@users.noreply.github.com>
2026-06-15 21:19:40 -0400

    Simplify App component

--- src/App.js
+++ src/App.js
@@ -5,19 +5,14 @@ import Routes from './components/Main/Routes';
 
 import { BrowserRouter as Router } from "react-router-dom";
 
-const App = props => {
-
-    let content = (
-        <Router>
-            <div>
-                <Navigation/>
-                <Routes/>
-            </div>
-        </Router>
-
-    );
-    return content;
-}
+const App = () => (
+    <Router>
+        <div>
+            <Navigation/>
+            <Routes/>
+        </div>
+    </Router>
+)
 
 
 export default App
